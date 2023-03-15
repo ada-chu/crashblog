28a49eb9011826ff32ce23e4f23a9be9e17acd95
Yi-Chu <83384846+ada-chu@users.noreply.github.com>
2023-03-15 15:25:49 +0800

    Update settings.py

--- crashblog/settings.py
+++ crashblog/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9n_ih-bhy$#92@-+)%zuxw_jilauy2wg313@p2rq3#fxxm20u+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
